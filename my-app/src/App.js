b77f98401dcca861583b82a5129122f37cbeb7e8
sarssath <srarssath@gmail.com>
2020-05-21 22:13:02 +0000

    API

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,8 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
-handleClick() {
-    console.log(alert("milan"));
+handleClick(event) {
+    console.log(event.target.id);
+    var city = event.target.id;
+    var country = event.target.dataset.country;
+    console.log(country);
+    fetch(`https://api.weatherbit.io/v2.0/current?city=${city}&country=${country}&units=i&key=8f9ffdb9a3bb404bb103448a39657671`)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function (response){
+	        console.log(response);
+        }
+    );
 }
 render() {
     return (
@@ -12,9 +23,9 @@ render() {
         <h1 id="weather">56</h1>
         <p>New York</p>
         <div className="cities">
-            <button id="London" onClick={this.handleClick}>London</button>
-            <button id="Dubai" onClick={this.handleClick}>Dubai</button>
-            <button id="Milan" onClick={this.handleClick}>Milan</button>
+            <button id="London" data-country="UK" onClick={this.handleClick}>London</button>
+            <button id="Dubai" data-country="AE" onClick={this.handleClick}>Dubai</button>
+            <button id="Milan" data-country="IT" onClick={this.handleClick}>Milan</button>
 
         </div>  
         </div>
